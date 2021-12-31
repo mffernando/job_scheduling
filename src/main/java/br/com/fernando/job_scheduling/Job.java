14794167aaa76e6acf15447300455fa6f9a14116
Fernando <fmti91@hotmail.com>
2021-12-31 10:34:05 -0300

    Job constructor and gettes

--- src/main/java/br/com/fernando/job_scheduling/Job.java
+++ src/main/java/br/com/fernando/job_scheduling/Job.java
@@ -11,4 +11,37 @@ public class Job {
 	private static String initialTime = "2019/11/10 09:00:00";
 	private static String endTime = "2019/11/11 12:00:00";
 
+	// constructor
+	public Job(int id, String description, String jobEndTime, String JobEstimatedExecution) {
+
+		this.id = id;
+		this.description = description;
+		this.jobEndTime = jobEndTime;
+		this.JobEstimatedExecution = JobEstimatedExecution;
+
+		System.out.println("Job successfully added -  id: " + this.id + " description: " + this.description
+				+ " Job End Time: " + this.jobEndTime + " Job Estimated Execution: " + this.JobEstimatedExecution);
+
+		// System.out.println("start: " + initialTime + " end: " + endTime);
+
+	}
+
+	// getters
+
+	public int getId() {
+		return this.id;
+	}
+
+	public String getJobEndTime() {
+		return this.jobEndTime;
+	}
+
+	public static String getInitialTime() {
+		return Job.initialTime;
+	}
+
+	public static String getEndTime() {
+		return Job.endTime;
+	}
+
 }
