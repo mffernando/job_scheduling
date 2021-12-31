65ccd1de6237b3c6a68339d095a453b366c8c235
Fernando <fmti91@hotmail.com>
2021-12-31 10:32:57 -0300

    Job attributes

--- src/main/java/br/com/fernando/job_scheduling/Job.java
+++ src/main/java/br/com/fernando/job_scheduling/Job.java
@@ -2,4 +2,13 @@ package br.com.fernando.job_scheduling;
 
 public class Job {
 
+	// attributes
+	private int id;
+	private String description;
+	private String jobEndTime;
+	private String JobEstimatedExecution;
+
+	private static String initialTime = "2019/11/10 09:00:00";
+	private static String endTime = "2019/11/11 12:00:00";
+
 }
